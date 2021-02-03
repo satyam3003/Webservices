371f2a862346d1e9b6cbdc2e69a735f15a21e30f
satyam3003 <75485302+satyam3003@users.noreply.github.com>
2021-02-04 00:09:00 +0530

    v_1.1

---
+++ home.js
@@ -0,0 +1,22 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 1250,
+    delay: (el, i) => 100 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 5000
+  });
+
+$(".client-r1").hover(function () {
+
+})
