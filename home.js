bd296dd45ada9da0ce81045fe4bf5f539c5e7494
satyam3003 <75485302+satyam3003@users.noreply.github.com>
2021-02-05 00:16:57 +0530

    v2.6

--- home.js
+++ home.js
@@ -17,6 +17,20 @@ anime.timeline({loop: true})
     delay: 5000
   });
 
-$(".client-r1").hover(function () {
 
-})
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml15 .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i
+  }).add({
+    targets: '.ml15',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 5000
+  });
